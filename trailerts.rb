d34472baa6aa9d388ffebc530cf7dd07b1c15e14
Breyten Ernsting <bje@dds.nl>
2013-12-05 12:15:39 +0100

    Factor out a method for getting things from the cache.

--- trailerts.rb
+++ trailerts.rb
@@ -23,6 +23,22 @@ def get_config
   IniFile.load('trailerts.ini')
 end
 
+def get_if_cached(cache_key, proc, cache_timeout = 43200)
+  redis = Redis.new
+  
+  cached_movies = redis[cache_key]
+  if cached_movies
+    movies = JSON.parse(cached_movies)
+  else
+    @tmdb = Tmdb::Api.key(@config['themoviedb']['key'])
+    movies = proc.call
+    redis[cache_key] = movies.to_json
+    redis.expire(cache_key, cache_timeout) # half a day
+  end
+  
+  movies
+end
+
 # Usage: partial :foo
 helpers do
   def partial(page, options={})
@@ -38,22 +54,19 @@ get '/' do
   erb :index
 end
 
+get '/now_playing' do
+  @slug = 'now_playing'
+  @config = get_config
+
+  erb :index
+end
+
 get '/api/now_playing' do
   response.headers['Content-type'] = "application/json"
   
   @config = get_config
   
-  redis = Redis.new
-
-  cached_movies = redis['trailerts_now_playing']
-  if cached_movies
-    movies = JSON.parse(cached_movies)
-  else
-    @tmdb = Tmdb::Api.key(@config['themoviedb']['key'])
-    movies = Tmdb::Movie.now_playing
-    redis['trailerts_now_playing'] = movies.to_json
-    redis.expire('trailerts_now_playing', 43200) # half a day
-  end
+  movies = get_if_cached('trailerts_now_playing', Proc.new { Tmdb::Movie.now_playing })
   movie = movies.sample
   movie.to_json
 end
@@ -63,17 +76,7 @@ get '/api/upcoming' do
   
   @config = get_config
   
-  redis = Redis.new
-
-  cached_movies = redis['trailerts_upcoming']
-  if cached_movies
-    movies = JSON.parse(cached_movies)
-  else
-    @tmdb = Tmdb::Api.key(@config['themoviedb']['key'])
-    movies = Tmdb::Movie.upcoming
-    redis['trailerts_upcoming'] = movies.to_json
-    redis.expire('trailerts_upcoming', 43200) # half a day
-  end
+  movies = get_if_cached('trailerts_upcoming', Proc.new { Tmdb::Movie.upcoming })
   movie = movies.sample
   movie.to_json
 end
@@ -90,17 +93,7 @@ get '/api/discover' do
 
   redis_key = 'trailerts_discover_%s' % @movie_params.hash.to_s
 
-  redis = Redis.new
-
-  cached_movies = redis[redis_key]
-  if cached_movies
-    movies = JSON.parse(cached_movies)
-  else
-    @tmdb = Tmdb::Api.key(@config['themoviedb']['key'])
-    movies = Tmdb::Movie.discover @movie_params #now_playing
-    redis[redis_key] = movies.to_json
-    redis.expire(redis_key, 43200) # half a day
-  end
+  movies = get_if_cached(redis_key, Proc.new { Tmdb::Movie.discover @movie_params })
   movie = movies.sample
   movie.to_json
 end
